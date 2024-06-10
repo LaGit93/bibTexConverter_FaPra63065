2a143f070de3449421f2444555fff2ec856c3653
Constantin Schulz <constantinschulz@gmx.de>
2024-06-10 19:54:25 +0200

    bibtexmagic.py erstellt, dort bitte Modell einfügen

---
+++ sourcen/bibtexmagic.py
@@ -0,0 +1,12 @@
+def create_bibtex(refstring):
+    # Hier die Funktion einfügen. Input und Output sind jeweils Strings
+
+    out_string = refstring [::-1]
+    out_string = out_string.upper()
+
+
+    return out_string
+
+
+# Hier können auch die Hilfsfunktionen hin
+
--- sourcen/main.py
+++ sourcen/main.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, request
-
+import bibtexmagic as btm
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/BibTexConverter', methods=['POST'])
 def modify():
     if request.method == 'POST':
         input_string = request.form['input_string']
-        modified_string = input_string.upper()  # Hier wird der eingegebene String in Großbuchstaben umgewandelt
-        
+        # modified_string = input_string.upper()  # Hier wird der eingegebene String in Großbuchstaben umgewandelt
+        modified_string = btm.create_bibtex(input_string)
         return render_template('result.html', input_string=input_string, modified_string=modified_string)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
